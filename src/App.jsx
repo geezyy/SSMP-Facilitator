18c29e061837a09c16667b212e5c6cffca230dd0
Ikujebi <133238121+Ikujebi@users.noreply.github.com>
2023-08-23 16:27:35 +0100

    login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import '../src/index.css';
 import Layout from './Components/Layout';
+import Login from './Pages/Login';
+import {Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div>
-      <Layout />
-    </div>
+    <Routes>
+      <Route path='/' element={<Login/>}/>
+      <Route path='/layout' element={<Layout/>}/>
+      
+    </Routes>
   );
 }
 
